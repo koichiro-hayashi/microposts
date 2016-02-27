98555aefeca12e9e97d1ee0e296a4e3ce882d5fb
koichiro_hayashi <koroichi0427@gmail.com>
2016-02-27 10:36:20 +0000

    add user model

--- db/migrate/20160227093226_create_uses.rb
+++ db/migrate/20160227093226_create_uses.rb
@@ -1,12 +1,12 @@
-class CreateUses < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :uses do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password_digest
 
       t.timestamps null: false
-      
+
       t.index :email, unique: true # この行を追加
     end
   end
