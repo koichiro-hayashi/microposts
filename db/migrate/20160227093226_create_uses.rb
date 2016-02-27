df27501e3cabf3e1c4702b0b761545b477c86286
koichiro_hayashi <koroichi0427@gmail.com>
2016-02-27 09:46:48 +0000

    add user model

---
+++ db/migrate/20160227093226_create_uses.rb
@@ -0,0 +1,13 @@
+class CreateUses < ActiveRecord::Migration
+  def change
+    create_table :uses do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+      
+      t.index :email, unique: true # この行を追加
+    end
+  end
+end
