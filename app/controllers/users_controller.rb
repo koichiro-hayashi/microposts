46fef06235ac41570ca3e96567ebdccf9fc1041a
koichiro_hayashi <koroichi0427@gmail.com>
2016-03-07 13:29:59 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :area,
                                  :password_confirmation)
   end
   
