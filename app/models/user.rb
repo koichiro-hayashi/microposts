9a1d8750fb8c9398fd76b42264193b283bcf9f52
koichiro_hayashi <koroichi0427@gmail.com>
2016-03-11 13:34:22 +0000

    presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { maximum: 30 }
-  validates :area, length: { maximum: 30 }
+  validates :area, presence: true, length: { maximum: 30 }
   has_secure_password
   has_many :microposts
   
